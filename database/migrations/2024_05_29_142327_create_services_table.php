8257019ce02a69b7d52136c573e54d4d1c3bb7d9
william victor <vickenconcept@gmail.com>
2024-05-30 07:25:21 -0700

    service view refactored

--- database/migrations/2024_05_29_142327_create_services_table.php
+++ database/migrations/2024_05_29_142327_create_services_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->longText('description');
             $table->enum('type',['cleaning','task'])->default('cleaning');
             $table->timestamps();
         });
